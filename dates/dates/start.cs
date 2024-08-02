342030cf67110352761cc6f6a8382d2c8f2f1af0
DeborahK <deborahk@insteptech.com>
2024-08-02 16:19:43 -0700

    Add more to start code

--- dates/dates/start.cs
+++ dates/dates/start.cs
@@ -4,3 +4,19 @@
 
 
 // Task 2: Track a time without a date
+
+// Find the amount of time between two times
+// TimeSpan numHoursSince = currentTime - alarmTime;
+// Console.WriteLine($"Hours since alarm: {numHoursSince.TotalHours:N0}");
+
+// TimeSpan numHoursUntil = alarmTime - currentTime;
+// Console.WriteLine($"Hours before the next alarm: {numHoursUntil.TotalHours:N0}");
+
+// Determine if a store is open
+// TimeOnly open = new(10,0,0);
+// TimeOnly close = new(18,0,0);
+
+// if (currentTime.IsBetween(open, close))
+//   Console.WriteLine("It's open!");
+// else
+//   Console.WriteLine("It's closed");
